43ef220c30713268e7827e0b10684000c5d0930a
anoduck <11767-anoduck@users.noreply.gitgud.io>
2021-11-27 22:25:20 -0500

    Attempting to resolve index out of range issue.

--- walli.py
+++ walli.py
@@ -14,7 +14,9 @@ from subprocess import call
 #####################
 CONFIG_DIR = os.path.expanduser('~/.config/walli/')
 DEFAULT_DIR = os.path.expanduser('~/.config/walli/images/')
-BACKGROUND_FILE = pywal.wallpaper.get()
+BACKGROUND_FILE = os.path.join(os.environ['HOME'], '.config/walli/background1')
+PREV_IMAGE = os.path.join(os.environ['HOME'], '.cache/wal/wal')
+
 
 ################################
 ## Kill other walli processes ##
@@ -40,16 +42,25 @@ def set_wall(args):
     TIME = float(args.t)
     #Sets directory to the image directory
     os.chdir(DIR)
+    ## Get File Size of previous
+    PRWALL = os.path.getsize(PREV_IMAGE)
     #Waits till the time is set
-    while True:
+    if PRWALL > 60:
+        WAL_SET = True
+    else:
+        WAL_SET = False
         #Performs the command after time.
+    if WAL_SET == False:
+        image = random.choice([
+        x for x in os.listdir(DEFAULT_DIR)
+        if os.path.isfile(os.path.join(DEFAULT_DIR, x))
+        ]) 
+        #  image = pywal.image.get(DIR)
+        pywal.wallpaper.change(image)
+    # Else wait for Time, check previous wallpaper
+    else:
         time.sleep(TIME)
-        #Selects a random image file
-        image_try = pywal.image.get(DIR)
-        while image_try == pywal.wallpaper.get():
-            image_try = pywal.image.get(DIR)
-        else:
-            image = image_try
+        image = pywal.image.get(DIR, DST1)
         pywal.wallpaper.change(image)
 
 ##########
@@ -57,10 +68,10 @@ def set_wall(args):
 ##########
 
 def main(**kwargs):
-    ap = argparse.ArgumentParser(description='Something fantastic', epilog='Here is an epilog')
+    ap = argparse.ArgumentParser(description='A Friendly Wallpaper Changer for I3 in Python', epilog='Rotates desktop wallpaper by defined time from defined directory with defined opacity.')
     ap.add_argument('-d', metavar='Working Directory', action='store_const', const=str, help='The directory containing images to use for wallpapers.', default=DEFAULT_DIR)
     ap.add_argument('-f', metavar='Current Wallpaper', type=argparse.FileType('w', encoding='UTF-8'), help='Destination to save current wallpaper', default=BACKGROUND_FILE)
-    ap.add_argument('-t', metavar='Time', type=int, help='Time between wallpaper changes', default=210)
+    ap.add_argument('-t', metavar='Time', type=int, help='Time between wallpaper changes', default=150)
     ap.add_argument('-o', metavar='Opacity', type=int, help='Opacity Level', default=85)
     args = ap.parse_args()
     kill_wall()
